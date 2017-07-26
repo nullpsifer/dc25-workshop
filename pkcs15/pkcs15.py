489aa3cf5457ddd9e85add7433b16b0f1e45c984
Matt Cheung <mgcheung@gmail.com>
2017-07-26 13:36:49 -0400

    adding a PKCS1.5 padding function

--- pkcs15/pkcs15.py
+++ pkcs15/pkcs15.py
@@ -1,3 +1,4 @@
+from hashlib import md5, sha1, sha256,sha384, sha512
 HASH_ASN1 = {
             'MD5': b'\x30\x20\x30\x0c\x06\x08\x2a\x86'
             b'\x48\x86\xf7\x0d\x02\x05\x05\x00\x04\x10',
@@ -10,3 +11,18 @@ HASH_ASN1 = {
             'SHA-512': b'\x30\x51\x30\x0d\x06\x09\x60\x86'
             b'\x48\x01\x65\x03\x04\x02\x03\x05\x00\x04\x40',
 }
+
+HASH_FUNCTION = {
+            'MD5': md5,
+            'SHA-1': sha1,
+            'SHA-256': sha256,
+            'SHA-384': sha384,
+            'SHA-512': sha512
+}
+
+def pkcs15pad(message, hashchoice, bitlength):
+    assert bitlength > 256 and bitlength % 256 == 0
+    digest = HASH_FUNCTION[hashchoice](message).digest()
+    bytelength = bitlength >> 3
+    flength = bytelength - 3 - len(digest) - len(HASH_ASN1[hashchoice])
+    return b'\x00\x01'+b'\xff'*flength+b'\x00'+HASH_ASN1[hashchoice]+digest
